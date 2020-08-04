09bc2af71c1861eed0aa9b1338da6253f459793b
Jatinmanav <46203753+Jatinmanav@users.noreply.github.com>
2020-08-04 22:15:44 +0530

    add canvas

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
 import React from "react";
 import "./styles/App.scss";
 import Header from "./components/Header";
+import Canvas from "./components/Canvas";
 
 const App: React.FC = () => {
   return (
     <div className="App">
       <Header />
+      <div className="container">
+        <Canvas />
+      </div>
     </div>
   );
 };
