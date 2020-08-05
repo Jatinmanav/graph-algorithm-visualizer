5b99dea5597ce1a0cca8e8e99f87c9bcbe927c50
Jatinmanav <46203753+Jatinmanav@users.noreply.github.com>
2020-08-05 22:03:21 +0530

    add useWindowSize

--- src/components/Canvas.tsx
+++ src/components/Canvas.tsx
@@ -1,8 +1,12 @@
 import React, { useState, useEffect, useRef } from "react";
+import useWindowSize from "../hooks/windowSize";
 
 const Canvas = () => {
   const canvasRef = useRef<HTMLCanvasElement>(null);
   const [canvas, setCanvas] = useState<HTMLCanvasElement | null>(null);
+  const [context, setContext] = useState<CanvasRenderingContext2D | null>(null);
+  const [width, height] = useWindowSize();
+  console.log(width, height);
 
   const handleClick = (clientX: number, clientY: number): void => {
     if (canvas) {
@@ -10,11 +14,22 @@ const Canvas = () => {
       const x = clientX - rect.left;
       const y = clientY - rect.top;
       console.log("x: " + x + " y: " + y);
+      if (context) {
+        context.beginPath();
+        context.arc(x, y, 10, 0, 2 * Math.PI, false);
+        context.fill();
+      }
     }
   };
 
   useEffect(() => {
-    setCanvas(canvasRef.current);
+    let canvas = canvasRef.current;
+    if (canvas) {
+      setCanvas(canvas);
+      setContext(canvas.getContext("2d"));
+      canvas.width = window.innerWidth;
+      canvas.height = window.innerHeight;
+    }
   }, []);
 
   return (
---
+++ src/hooks/windowSize.ts
@@ -0,0 +1,15 @@
+import { useState, useLayoutEffect } from "react";
+
+const useWindowSize = () => {
+  const [size, setSize] = useState([0, 0]);
+  useLayoutEffect(() => {
+    const updateSize = (): void => {
+      setSize([window.innerWidth, window.innerHeight]);
+    };
+    window.addEventListener("resize", updateSize);
+    return () => window.removeEventListener("resize", updateSize);
+  }, []);
+  return size;
+};
+
+export default useWindowSize;
