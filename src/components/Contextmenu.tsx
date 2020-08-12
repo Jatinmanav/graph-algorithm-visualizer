cccbbd3c674095081829a80f7f52b19262f4216e
Jatinmanav <46203753+Jatinmanav@users.noreply.github.com>
2020-08-12 23:44:48 +0530

    update context menu

--- src/components/Contextmenu.tsx
+++ src/components/Contextmenu.tsx
@@ -6,13 +6,21 @@ type AppProps = { contextmenu: contextMenu };
 const Contextmenu = ({ contextmenu }: AppProps) => {
   //eslint-disable-next-line
   const { isOpen, x, y } = contextmenu;
+  let innerX = x;
+  let innerY = y;
+  if (x + 200 > window.innerWidth) {
+    innerX = x - 200;
+  }
+  if (y + 300 > window.innerHeight) {
+    innerY = y - 300;
+  }
   console.log(x, y);
   return (
     <div
       className="context-menu"
-      style={{ left: x, top: y, position: "absolute" }}
+      style={{ left: innerX, top: innerY, position: "absolute" }}
     >
-      <h1>This is the context menu</h1>
+      <h3>This is the context menu</h3>
     </div>
   );
 };
