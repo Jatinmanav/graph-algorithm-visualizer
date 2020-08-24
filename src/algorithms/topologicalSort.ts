82e21c3f6e7a12a5235d08f5e3398af59c6db0b9
Jatinmanav <46203753+Jatinmanav@users.noreply.github.com>
2020-08-24 23:47:24 +0530

    add topological sort

--- src/algorithms/topologicalSort.ts
+++ src/algorithms/topologicalSort.ts
@@ -2,12 +2,29 @@ const dfs = (
   adjacencyList: number[][],
   index: number,
   visited: Set<number>,
+  tempVisited: Set<number>,
   result: number[]
 ): boolean => {
   visited.add(index);
+  tempVisited.add(index);
+  if (adjacencyList[index].length === 0) {
+    tempVisited.clear();
+  }
   for (let item of adjacencyList[index]) {
+    if (tempVisited.has(item) === true) {
+      return false;
+    }
     if (visited.has(item) === false) {
-      dfs(adjacencyList, item, visited, result);
+      let value: boolean = dfs(
+        adjacencyList,
+        item,
+        visited,
+        tempVisited,
+        result
+      );
+      if (value === false) {
+        return false;
+      }
     }
   }
   result.push(index);
@@ -18,8 +35,19 @@ const topologicalSort = (adjacencyList: number[][]): number[] => {
   let result: number[] = [];
   let visited = new Set<number>();
   for (let item in adjacencyList) {
+    let tempVisited = new Set<number>();
     if (visited.has(+item) === false) {
-      dfs(adjacencyList, +item, visited, result);
+      let value: boolean = dfs(
+        adjacencyList,
+        +item,
+        visited,
+        tempVisited,
+        result
+      );
+      if (value === false) {
+        result = [];
+        return result;
+      }
     }
   }
   return result;
