fa40fe2f16908f6da4a8b6c70a903e8b542db4a1
Jatinmanav <46203753+Jatinmanav@users.noreply.github.com>
2020-08-26 21:56:17 +0530

    update draw edge

--- src/actions/drawEdge.ts
+++ src/actions/drawEdge.ts
@@ -5,10 +5,26 @@ const drawEdge = (
   target: node,
   context: CanvasRenderingContext2D
 ): void => {
-  const sourceX = source.canvasX;
-  const sourceY = source.canvasY;
-  const targetX = target.canvasX;
-  const targetY = target.canvasY;
+  let sourceX = source.canvasX;
+  let sourceY = source.canvasY;
+  let targetX = target.canvasX;
+  let targetY = target.canvasY;
+  const theta = Math.atan((targetY - sourceY) / (targetX - sourceX));
+  console.log(theta);
+  if (
+    (targetY - sourceY > 0 && targetX - sourceX > 0) ||
+    (targetY - sourceY < 0 && targetX - sourceX > 0)
+  ) {
+    targetX = targetX - 20 * Math.cos(theta);
+    targetY = targetY - 20 * Math.sin(theta);
+    sourceX = sourceX + 20 * Math.cos(theta);
+    sourceY = sourceY + 20 * Math.sin(theta);
+  } else {
+    targetX = targetX + 20 * Math.cos(theta);
+    targetY = targetY + 20 * Math.sin(theta);
+    sourceX = sourceX - 20 * Math.cos(theta);
+    sourceY = sourceY - 20 * Math.sin(theta);
+  }
   context.lineWidth = 3;
   context.beginPath();
   context.moveTo(sourceX, sourceY);
