7361dad9df94d3f933741529b9a53b6e9294e7fd
Jatinmanav <46203753+Jatinmanav@users.noreply.github.com>
2020-09-09 21:57:19 +0530

    add breadth first traversal

---
+++ src/algorithms/breadthFirstTraversal.ts
@@ -0,0 +1,10 @@
+import adjacencyListObject from "../types/adjacencyListObject";
+
+const breadthFirstTraversal = (
+  adjacencyList: adjacencyListObject
+): number[] => {
+  let result: number[] = [];
+  return result;
+};
+
+export default breadthFirstTraversal;
