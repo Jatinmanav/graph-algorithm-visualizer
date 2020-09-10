329f22bdeefce8b119e99e09c12668edaa71ac13
Jatinmanav <46203753+Jatinmanav@users.noreply.github.com>
2020-09-10 21:07:54 +0530

    update breadth first traversal

--- src/algorithms/breadthFirstTraversal.ts
+++ src/algorithms/breadthFirstTraversal.ts
@@ -1,9 +1,29 @@
 import adjacencyListObject from "../types/adjacencyListObject";
 
 const breadthFirstTraversal = (
-  adjacencyList: adjacencyListObject
+  adjacencyList: adjacencyListObject[]
 ): number[] => {
   let result: number[] = [];
+  let visited: Set<number> = new Set<number>();
+  let queue: number[] = [];
+  visited.add(adjacencyList[0].count);
+  queue.push(adjacencyList[0].count);
+  while (queue.length > 0) {
+    let temp: number = queue[0];
+    queue.shift();
+    result.push(temp);
+    for (let item of adjacencyList) {
+      if (item.count === temp) {
+        for (let iter of item.target) {
+          if (!visited.has(iter)) {
+            visited.add(iter);
+            queue.push(iter);
+          }
+        }
+      }
+    }
+  }
+
   return result;
 };
 
--- src/components/Visualize.tsx
+++ src/components/Visualize.tsx
@@ -1,4 +1,4 @@
-import React, { useContext } from "react";
+import React, { useState, useContext } from "react";
 import "../styles/Visualize.scss";
 import visualizeAlgorithm from "../actions/visualizeAlgorithm";
 import topologicalSort from "../algorithms/topologicalSort";
@@ -7,16 +7,23 @@ import adjacencyListProvider from "../types/adjacencyListProvider";
 import canvasProvider from "../types/canvasProvider";
 import node from "../types/node";
 import { CanvasContext } from "../contexts/CanvasContext";
+import breadthFirstTraversal from "../algorithms/breadthFirstTraversal";
 
 const Visualize = () => {
   const { nodeList, edgeList, adjacencyList } = useContext<
     adjacencyListProvider
   >(AdjacencyListContext);
   const { canvas, context } = useContext<canvasProvider>(CanvasContext);
+  const [algorithm, setAlgorithm] = useState<number>(1);
 
   const handleVisualize = (event: React.FormEvent<HTMLDivElement>) => {
     event.preventDefault();
-    const result = topologicalSort(adjacencyList, nodeList);
+    let result: number[] = [];
+    if (algorithm === 1) {
+      result = topologicalSort(adjacencyList, nodeList);
+    } else if (algorithm === 2) {
+      result = breadthFirstTraversal(adjacencyList);
+    }
     let resultNodes: node[] = [];
     for (let item of result) {
       for (let item2 of nodeList) {
@@ -28,10 +35,25 @@ const Visualize = () => {
     visualizeAlgorithm(resultNodes, edgeList, canvas, context);
   };
 
+  const handleSelectChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
+    event.preventDefault();
+    setAlgorithm(+event.target.value);
+  };
+
   return (
     <div className="visualize-container">
-      <select name="algorithm" className="visualize-select">
-        <option className="visualize-option">Topological Sort</option>
+      <select
+        name="algorithm"
+        className="visualize-select"
+        value={algorithm}
+        onChange={handleSelectChange}
+      >
+        <option className="visualize-option" value={1}>
+          Topological Sort
+        </option>
+        <option className="visualize-option" value={2}>
+          Breadth First Traversal
+        </option>
         {/* <option className="visualize-option"></option>
         <option className="visualize-option"></option>
         <option className="visualize-option"></option>
