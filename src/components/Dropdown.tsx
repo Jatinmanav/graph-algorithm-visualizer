9afb5180c089609d1209794ba2fee6bc5bbdd98e
Jatinmanav <46203753+Jatinmanav@users.noreply.github.com>
2020-09-14 20:55:47 +0530

    update header

---
+++ src/components/Dropdown.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import node from "../types/node";
+
+type AppProps = { nodeList: node[]; setNode(value: number): void };
+
+const Dropdown = ({ nodeList, setNode }: AppProps) => {
+  return <div></div>;
+};
+
+export default Dropdown;
