951719d85608bdccaa2f00dc785f393b633d0e53
Jatinmanav <46203753+Jatinmanav@users.noreply.github.com>
2020-09-16 22:40:30 +0530

    update dropdown menu

--- src/components/Dropdown.tsx
+++ src/components/Dropdown.tsx
@@ -1,10 +1,26 @@
 import React from "react";
 import node from "../types/node";
 
-type AppProps = { nodeList: node[]; setNode(value: number): void };
+type AppProps = {
+  nodeList: node[];
+  count: number;
+  setNode(value: number): void;
+};
 
-const Dropdown = ({ nodeList, setNode }: AppProps) => {
-  return <div></div>;
+const Dropdown = ({ nodeList, count, setNode }: AppProps) => {
+  return (
+    <div className="dropdown-container">
+      <ul className="dropdown-ul">
+        {nodeList.map((item) => {
+          if (item.count !== count) {
+            return <li>Hello</li>;
+          } else {
+            return <React.Fragment />;
+          }
+        })}
+      </ul>
+    </div>
+  );
 };
 
 export default Dropdown;
