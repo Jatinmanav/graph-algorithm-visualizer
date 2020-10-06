79623321b6efc51c346bfab29a05f947cdb9b8ec
Jatinmanav <46203753+Jatinmanav@users.noreply.github.com>
2020-10-06 21:08:53 +0530

    update snackbar

--- src/components/Snackbar.tsx
+++ src/components/Snackbar.tsx
@@ -2,6 +2,7 @@ import React, { useContext } from "react";
 import { SnackbarContext } from "../contexts/SnackbarContext";
 import "../styles/Snackbar.scss";
 import snackbarProvider from "../types/snackbarProvider";
+import { ReactComponent as CloseIcon } from "../icons/close.svg";
 
 const Snackbar = () => {
   const { open, message } = useContext<snackbarProvider>(SnackbarContext);
@@ -10,6 +11,7 @@ const Snackbar = () => {
       {open ? (
         <div className="snackbar-container">
           <p className="snackbar-text">{message}</p>
+          <CloseIcon className="snackbar-close-icon" />
         </div>
       ) : (
         <React.Fragment />
