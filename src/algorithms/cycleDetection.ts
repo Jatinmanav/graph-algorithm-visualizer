4a9c2cc89b3397817324656f5a68d91086e7adee
Jatinmanav <46203753+Jatinmanav@users.noreply.github.com>
2020-10-25 22:34:54 +0530

    add cycle detection algorithm

---
+++ src/algorithms/cycleDetection.ts
@@ -0,0 +1,8 @@
+import adjacencyListObject from "../types/adjacencyListObject";
+
+const cycleDetection =(adjacencyList: adjacencyListObject): number[]=> {
+  let result: number[] = [];
+  return result;
+}
+
+export default cycleDetection;
