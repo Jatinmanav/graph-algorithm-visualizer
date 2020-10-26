503bc21ed245d55fa6718cb3e3cf73a9380339a2
Jatinmanav <46203753+Jatinmanav@users.noreply.github.com>
2020-10-26 21:55:50 +0530

    update cycle detection algo

--- src/algorithms/cycleDetection.ts
+++ src/algorithms/cycleDetection.ts
@@ -1,8 +1,40 @@
 import adjacencyListObject from "../types/adjacencyListObject";
 
-const cycleDetection =(adjacencyList: adjacencyListObject): number[]=> {
+const cycleDetectionHelper = (
+  adjacencyList: adjacencyListObject[],
+  count: number,
+  visited: Set<number>,
+  recStack: Set<number>
+): boolean => {
+  if (visited.has(count) === false) {
+    visited.add(count);
+    recStack.add(count);
+    for (let item of adjacencyList) {
+      if (item.count === count) {
+        for (let iter of item.target) {
+          if (
+            !visited.has(count) &&
+            cycleDetectionHelper(adjacencyList, iter, visited, recStack)
+          )
+            return true;
+          else if (recStack.has(iter)) return true;
+        }
+      }
+    }
+  }
+  recStack.delete(count);
+  return false;
+};
+
+const cycleDetection = (adjacencyList: adjacencyListObject[]): number[] => {
   let result: number[] = [];
+  let visited: Set<number> = new Set();
+  let recStack: Set<number> = new Set();
+  for (let item of adjacencyList) {
+    if (cycleDetectionHelper(adjacencyList, item.count, visited, recStack))
+      return result;
+  }
   return result;
-}
+};
 
 export default cycleDetection;
