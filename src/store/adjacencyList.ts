e0c25a630de2ef48e3a9cb71b57473875b77368f
Jatinmanav <46203753+Jatinmanav@users.noreply.github.com>
2020-11-14 23:31:06 +0530

    add redux tests

--- src/store/adjacencyList.ts
+++ src/store/adjacencyList.ts
@@ -19,11 +19,27 @@ const adjacencyListSlice = createSlice({
   name: "adjacencyList",
   initialState,
   reducers: {
-    addNode: (state, action: PayloadAction<node>) => {
+    ADD_NODE: (state, action: PayloadAction<node>) => {
+      action.payload.count = 0;
+      if (state.nodeList.length > 1)
+        action.payload.count =
+          state.nodeList[state.nodeList.length - 1].count + 1;
       state.nodeList.push(action.payload);
+      state.adjacencyList.push({ count: action.payload.count, target: [] });
     },
+
+    DELETE_NODE: (state, action: PayloadAction<number>) => {},
+
+    ADD_EDGE: (state, action: PayloadAction<number>) => {},
+
+    DELETE_EDGE: (state, action: PayloadAction<number>) => {},
   },
 });
 
-export const { addNode } = adjacencyListSlice.actions;
+export const {
+  ADD_NODE,
+  DELETE_NODE,
+  ADD_EDGE,
+  DELETE_EDGE,
+} = adjacencyListSlice.actions;
 export default adjacencyListSlice.reducer;
--- src/test/store.test.ts
+++ src/test/store.test.ts
@@ -1,18 +1,68 @@
 import { EnhancedStore } from "@reduxjs/toolkit";
 import createNode from "../actions/createNode";
-import { addNode } from "../store/adjacencyList";
+import {
+  ADD_NODE,
+  DELETE_NODE,
+  ADD_EDGE,
+  DELETE_EDGE,
+} from "../store/adjacencyList";
 import configureStore from "../store/configureStore";
 
 let store: EnhancedStore | undefined;
+
+const adjacencyListState = () => {
+  return store?.getState().adjacencyList;
+};
 
 beforeEach(() => {
   store = configureStore();
 });
 
-test("node is added to nodeList when addNode is called", () => {
+test("node is added to nodeList and adjacencyList when ADD_NODE is called", () => {
   const node = createNode(0, 100, 100, 100, 100, 100, 100, "");
 
-  store?.dispatch(addNode(node));
+  store?.dispatch(ADD_NODE(node));
 
-  expect(store?.getState().adjacencyList.nodeList).toHaveLength(1);
+  expect(adjacencyListState().nodeList).toHaveLength(1);
+  expect(adjacencyListState().adjacencyList).toHaveLength(1);
+});
+
+test("node is deleted from nodeList, adjacencyList and edgeList when DELETE_NODE is called", () => {
+  const node = createNode(0, 100, 100, 100, 100, 100, 100, "");
+  const node2 = createNode(0, 100, 100, 100, 100, 100, 100, "");
+  const node3 = createNode(0, 100, 100, 100, 100, 100, 100, "");
+  const node4 = createNode(0, 100, 100, 100, 100, 100, 100, "");
+  const node5 = createNode(0, 100, 100, 100, 100, 100, 100, "");
+  const node6 = createNode(0, 100, 100, 100, 100, 100, 100, "");
+  const node7 = createNode(0, 100, 100, 100, 100, 100, 100, "");
+  const node8 = createNode(0, 100, 100, 100, 100, 100, 100, "");
+
+  store?.dispatch(ADD_NODE(node));
+  store?.dispatch(ADD_NODE(node2));
+  store?.dispatch(DELETE_NODE(0));
+  store?.dispatch(ADD_NODE(node3));
+  store?.dispatch(ADD_NODE(node4));
+  store?.dispatch(DELETE_NODE(2));
+  store?.dispatch(ADD_NODE(node5));
+  store?.dispatch(ADD_NODE(node6));
+  store?.dispatch(DELETE_NODE(5));
+  store?.dispatch(ADD_NODE(node7));
+  store?.dispatch(ADD_NODE(node8));
+
+  expect(adjacencyListState().nodeList[0].count).toEqual(1);
+  expect(adjacencyListState().nodeList[1].count).toEqual(3);
+  expect(adjacencyListState().nodeList[2].count).toEqual(4);
+  expect(adjacencyListState().nodeList[3].count).toEqual(5);
+  expect(adjacencyListState().nodeList[4].count).toEqual(6);
+});
+
+test("edge is added to edgeList and adjacencyList when ADD_EDGE is called", () => {
+  const node = createNode(0, 100, 100, 100, 100, 100, 100, "");
+  const node2 = createNode(0, 100, 100, 100, 100, 100, 100, "");
+  const node3 = createNode(0, 100, 100, 100, 100, 100, 100, "");
+  const node4 = createNode(0, 100, 100, 100, 100, 100, 100, "");
+  const node5 = createNode(0, 100, 100, 100, 100, 100, 100, "");
+  const node6 = createNode(0, 100, 100, 100, 100, 100, 100, "");
+  const node7 = createNode(0, 100, 100, 100, 100, 100, 100, "");
+  const node8 = createNode(0, 100, 100, 100, 100, 100, 100, "");
 });
