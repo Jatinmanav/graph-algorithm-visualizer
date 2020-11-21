03e004b8301e9a367579255778208c7b7e68441e
Jatinmanav <46203753+Jatinmanav@users.noreply.github.com>
2020-11-21 18:04:08 +0530

    add snackbar reducer

--- src/store/snackbar.ts
+++ src/store/snackbar.ts
@@ -1,9 +1,38 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+
+type SliceState = {
+  open: boolean;
+  message: string;
+};
+
+const initialState: SliceState = {
+  open: false,
+  message: "",
+};
 
 const snackbarSlice = createSlice({
   name: "snackbar",
-  initialState: {},
-  reducers: {},
+  initialState,
+  reducers: {
+    CLEAR_MESSAGE: () => {
+      return {
+        open: false,
+        message: "",
+      };
+    },
+
+    SET_MESSAGE: (state, action: PayloadAction<string>) => {
+      return {
+        open: true,
+        message: action.payload,
+      };
+    },
+  },
 });
 
+export const getSnackBarStateReducer = (state: SliceState) => state.open;
+
+export const getSnackBarMessageReducer = (state: SliceState) => state.message;
+
+export const { CLEAR_MESSAGE, SET_MESSAGE } = snackbarSlice.actions;
 export default snackbarSlice.reducer;
