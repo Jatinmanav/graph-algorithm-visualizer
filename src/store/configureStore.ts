be8a7d3082bac110e87513245007312cdbcd18ea
Jatinmanav <46203753+Jatinmanav@users.noreply.github.com>
2020-11-22 20:30:10 +0530

    remove canvas reducer

--- src/store/configureStore.ts
+++ src/store/configureStore.ts
@@ -1,11 +1,9 @@
 import { configureStore } from "@reduxjs/toolkit";
-import adjacencyListReducer from "../store/adjacencyList";
 import snackbarReducer from "../store/snackbar";
 
 export default () =>
   configureStore({
     reducer: {
-      adjacencyList: adjacencyListReducer,
       snackbar: snackbarReducer,
     },
   });
